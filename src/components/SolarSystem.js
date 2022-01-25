e63b2542f5c6e9aaa09f23f12f2ceafd2a8afe74
Guilherme Carvalho <guil_car@hotmail.com>
2022-01-25 17:43:48 -0300

    requisito 6

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
-function SolarSystem() {
-  return (
-    <div data-testid="solar-system">
-      teste
-    </div>
-  );
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+        <section>
+          {planets.map((planet) => (
+            <PlanetCard
+              key={ planet.name }
+              planetName={ planet.name }
+              planetImage={ planet.image }
+            />
+          ))}
+        </section>
+      </div>
+    );
+  }
 }
 
 export default SolarSystem;
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-function Title() {
-  const headline = 'planets';
-  return (
-    <h2>
-      {headline}
-    </h2>
-  );
+class Title extends React.Component {
+  render() {
+    const { headline } = this.props;
+    return (
+      <h2>
+        { headline }
+      </h2>
+    );
+  }
 }
 
 export default Title;
